27bc01015c9bfc0ee778315b5675a69edff25e25
김민수 <igotaspot426@gmail.com>
2025-02-01 22:14:17 +0900

    update

--- src/layouts/RootLayout/ThemeProvider/Global/index.tsx
+++ src/layouts/RootLayout/ThemeProvider/Global/index.tsx
@@ -20,16 +20,17 @@ export const Global = () => {
           cursor: url(https://cur.cursors-4u.net/nature/nat-6/nat600.cur), auto !important;
           -webkit-animation: cursor 1000ms infinite;
           animation: cursor 1000ms infinite;
-
+  
         }
 
         * {
-          color-scheme: ${theme.scheme};
-          box-sizing: border-box;
-          cursor: url(https://cur.cursors-4u.net/nature/nat-6/nat600.cur), auto !important;
-          -webkit-animation: cursor 1000ms infinite;
-          animation: cursor 1000ms infinite;
-        }
+        color-scheme: ${theme.scheme};
+        box-sizing: border-box;
+        cursor: url(frame1.png), auto;
+        -webkit-animation: cursor 1000ms infinite;
+        animation: cursor 1000ms infinite;
+
+      }
 
       @-webkit-keyframes cursor {
         0% {cursor: url(/cursor_ani/frame_00_delay-0.01s.png), auto;}
@@ -80,7 +81,7 @@ export const Global = () => {
         100% {cursor: url(/cursor_ani/frame_20_delay-0.01s.png), auto;}
 
       }
-        
+
         * {
           color-scheme: ${theme.scheme};
           box-sizing: border-box;
@@ -99,7 +100,7 @@ export const Global = () => {
 
         a {
           all: unset;
-          cursor: pointer;
+          cursor: url(https://cur.cursors-4u.net/nature/nat-6/nat600.cur), auto !important;
         }
 
         ul {
