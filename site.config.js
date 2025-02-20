02d885a59548058e31db2d4c6389d7dc4b833e97
김민수 <igotaspot426@gmail.com>
2025-02-20 10:31:18 +0900

    update

--- site.config.js
+++ site.config.js
@@ -48,7 +48,7 @@ const CONFIG = {
     },
   },
   naverSearchAdvisor: {
-    enable: true,
+    enable: false,
     config: {
       siteVerification: process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION || "",
     },
